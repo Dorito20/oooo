09d672fc28eeabfebfbaf27a298a1f254c0743bb
Dario Suarez Garcia <dario.suarez@tecnologicoliceocristiano.edu.ec>
2024-08-27 21:04:34 -0500

    Add files via upload

---
+++ CajaBanco.py
@@ -0,0 +1,27 @@
+# #definir el diccionario para almacenar los valores por usuario
+# usuarios = {}
+# #definir datos de prueba
+# usuarios["maria"] = 500.00
+# usuarios["jaun"] = 600.00
+
+# def depositar():
+#     nombre = input("Introduce el nombre del usuario: ")
+#     monto = float(input("Introduce el monto a depositar"))
+#     usuarios[nombre] += monto
+           
+# print(usuarios)
+      
+# depositar()
+
+# print(usuarios)     
+
+
+usuarios = []
+cuenta_bancaria = []
+
+usuario = {"nombre":"Gustavo","estado": "A"}
+usuario.append(usuario)
+
+trasaccion = {"nombre":"Gustavo","monto":400.00}
+cuenta_bancaria.append(trasaccion)
+print(cuenta_bancaria)
